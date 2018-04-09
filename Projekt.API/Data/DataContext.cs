a7b0eaaf21157813dbdf5ea827fc8e1f92ded78e
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-04-09 14:04:18 +0200

    Start with .net core backend

---
+++ Projekt.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Projekt.API.Models;
+
+namespace Projekt.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+
+        
+    }
+}
