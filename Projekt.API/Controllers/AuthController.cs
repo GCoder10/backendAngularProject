288b0484f6780aa0788e2477b7a4545488a3a9c3
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-04-12 20:37:53 +0200

    Autoryzacja
    Logowanie
    
    Rejestracja
    
    Przyznanie tokenu dla zalogowanego uzytkownika
    
    Ustalenie, w jakich przypadkach (wstepnie) bedzie wymagany token przez backend
    
    Kurs

--- Projekt.API/Controllers/AuthController.cs
+++ Projekt.API/Controllers/AuthController.cs
@@ -1,5 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Projekt.API.Data;
 using Projekt.API.Dtos;
 using Projekt.API.Models;
@@ -10,8 +15,10 @@ namespace Projekt.API.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
 
         }
@@ -22,7 +29,7 @@ namespace Projekt.API.Controllers
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
-               ModelState.AddModelError("Username", "Username already exists");
+                ModelState.AddModelError("Username", "Username already exists");
 
 
             // validate request
@@ -39,6 +46,38 @@ namespace Projekt.API.Controllers
 
             return StatusCode(201);
 
+        }
+
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // generate token
+            // JSON Web Token
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                }),
+                Expires = System.DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+
+            return Ok(new { tokenString });
         }
     }
 }
--- Projekt.API/Controllers/ValuesController.cs
+++ Projekt.API/Controllers/ValuesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Projekt.API.Data;
 
 namespace Projekt.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
