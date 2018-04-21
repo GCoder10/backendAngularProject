42f64a1cf3b80f09bffc9306871a73f553888fe3
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-04-21 21:02:22 +0200

    Dostosowanie backendu do przyjmowania danych dodanego pracownika w API

--- Projekt.API/Controllers/WorkersController.cs
+++ Projekt.API/Controllers/WorkersController.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Projekt.API.Data;
+using Projekt.API.Dtos;
+using Projekt.API.Models;
 
 namespace Projekt.API.Controllers
 {
@@ -12,7 +14,6 @@ namespace Projekt.API.Controllers
         public WorkersController(DataContext context)
         {
             _context = context;
-
         }
 
         [HttpGet]
@@ -21,6 +22,34 @@ namespace Projekt.API.Controllers
             var workers = await _context.workers.ToListAsync();
 
             return Ok(workers);
+        }
+
+        [HttpPost("addNewWorkers")]
+
+        public async Task<IActionResult> addWorkers([FromBody]WorkersForAddToLocalDatabaseDto workersForAddToLocalDatabaseDto)
+        {
+
+                var workerToCreate = new Worker
+                    {
+                        surname = workersForAddToLocalDatabaseDto.surname,
+                        name = workersForAddToLocalDatabaseDto.name,
+                        city = workersForAddToLocalDatabaseDto.city,
+                        email = workersForAddToLocalDatabaseDto.email,
+                        pass = workersForAddToLocalDatabaseDto.pass,
+                        pesel = workersForAddToLocalDatabaseDto.pesel,
+                        street = workersForAddToLocalDatabaseDto.street
+                    };
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+        
+
+
+                await _context.workers.AddAsync(workerToCreate);
+                await _context.SaveChangesAsync();
+                return StatusCode(201);
+
         }
         
     }
---
+++ Projekt.API/Dtos/WorkersForAddToLocalDatabaseDto.cs
@@ -0,0 +1,13 @@
+namespace Projekt.API.Dtos
+{
+    public class WorkersForAddToLocalDatabaseDto
+    {
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public string email { get; set; }
+        public string pass { get; set; }
+        public string pesel { get; set; }
+        public string street { get; set; }
+    }
+}
