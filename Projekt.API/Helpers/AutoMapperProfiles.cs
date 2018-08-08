176b0f3b607653aa11cf8a84928a3d4647eede91
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-08-08 22:06:42 +0200

    database, save data from form
    database, save data from form

--- Projekt.API/Helpers/AutoMapperProfiles.cs
+++ Projekt.API/Helpers/AutoMapperProfiles.cs
@@ -26,6 +26,7 @@ namespace Projekt.API.Helpers
                 });
 
             CreateMap<Photo, PhotosForDetailedDto>(); 
+            CreateMap<UserForUpdateDto, User>();
         }   
     }
 }
