05b3a91e6a1547127868348c5aff4603b94bf339
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-09-03 20:15:09 +0200

    Add new query params - UserParams
    Add new query params for take the users we like and who like us.
    If likers or likees = true in query params, then prepare users who like us / we like.

--- Projekt.API/Data/ApplicationRepository.cs
+++ Projekt.API/Data/ApplicationRepository.cs
@@ -58,6 +58,18 @@ namespace Projekt.API.Data
 
             users = users.Where(u => u.Gender == userParams.Gender);
 
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
+
             if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
                 var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
@@ -80,6 +92,22 @@ namespace Projekt.API.Data
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users.Include(x => x.Liker)
+                                           .Include(x => x.Likees)
+                                           .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+            {
+                return user.Liker.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+            }
+            else
+            {
+                return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+            }
         }
 
         public async Task<bool> SaveAll()
